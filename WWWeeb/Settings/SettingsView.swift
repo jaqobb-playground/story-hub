ead0eab473f66175b84ef0c4f4da8a65727335fd
Jakub Zagórski <jaqobbdev@gmail.com>
2024-04-17 00:18:53 +0200

    Add information about app version and build to settings

--- WWWeeb/Settings/SettingsView.swift
+++ WWWeeb/Settings/SettingsView.swift
@@ -3,12 +3,39 @@ import SwiftUI
 struct SettingsView: View {
     @EnvironmentObject
     var libraryStore: LibraryStore
-    
+
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
+                Section(header: Text("About")) {
+                    LabeledContent {
+                        Text(getAppVersion())
+                    } label: {
+                        Text("Version")
+                    }
+                    
+                    LabeledContent {
+                        Text(getAppBuild())
+                    } label: {
+                        Text("Build")
+                    }
+                }
             }
             .navigationTitle("Settings")
         }
+    }
+
+    private func getAppVersion() -> String {
+        if let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            return version
+        }
+        return "Unknown"
+    }
+    
+    private func getAppBuild() -> String {
+        if let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
+            return build
+        }
+        return "Unknown"
     }
 }
